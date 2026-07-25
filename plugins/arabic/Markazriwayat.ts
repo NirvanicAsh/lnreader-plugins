e902070ffc3b9b4b7241360af30a0a2f11970f85
hamedhani1998 <hanihameda1998@gmail.com>
2026-07-25 18:02:40 +0300

    Update plugins/arabic/Markazriwayat.ts
    Co-authored-by: K1ngfish3r <26593485+K1ngfish3r@users.noreply.github.com>

--- plugins/arabic/Markazriwayat.ts
+++ plugins/arabic/Markazriwayat.ts
@@ -15,7 +15,7 @@ class Markazriwayat implements Plugin.PluginBase {
   private UA =
     'Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36';
 
-  filters: Filters = {
+  filters = {
     order: {
       type: FilterTypes.Picker,
       label: 'الترتيب',
