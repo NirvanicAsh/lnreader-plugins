34d163a2edd1af79d53e9195bf197c6d4d208aa9
NirvanicAsh <60535087+NirvanicAsh@users.noreply.github.com>
2026-08-09 11:36:17 +0800

    Update Pen&Paper.ts

--- plugins/en/Pen&Paper.ts
+++ plugins/en/Pen&Paper.ts
@@ -6,7 +6,7 @@ class PenAndPaperTranslations implements Plugin.PluginBase {
   id = 'penpaper';
   name = 'Pen and Paper Translations';
   site = 'https://penandpapertranslations.com/';
-  version = '1.0.2';
+  version = '1.0.3';
   icon = 'src/en/penpaper/icon.png';
 
   coversCache?: Map<string, string>;
